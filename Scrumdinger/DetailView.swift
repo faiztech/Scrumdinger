3d95cdfdc5b8eea8489892e47343e30adbcac553
faiztech <mohammedfaizuddin@live.com>
2020-12-15 22:30:17 -0500

    Creating a Navigation Heirarchy

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Mohammed Faizuddin on 15/12/2020.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                        .accessibilityLabel(Text("Start Meeting"))
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting Length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(scrum.color)
+                }
+            }
+
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
